07c09a7d4456645df84f6d8191fa19cea58a3709
sushidesu <hugemerge@gmail.com>
2021-04-18 20:15:24 +0900

    仮のページを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head"
 
 export function Index(): JSX.Element {
   return (
-    <div>
-      <h1 className="text-2xl">パフェを食べたい</h1>
+    <div className="bg-blue-400 max-w-lg mx-auto my-6 p-3 rounded hover:shadow-lg">
+      <Head>
+        <title>シメパフェどう？後悔？公式サイト</title>
+      </Head>
+      <h1 className="text-2xl font-bold">パフェを食べたい</h1>
     </div>
   )
 }
